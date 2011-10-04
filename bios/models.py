9385e3426d617eb4b718b214b36f24fc1f627279
Chris Edgemon <chrisedgemon@gmail.com>
2011-10-04 14:40:50 -0400

    Add Game and Bio models.

--- bios/models.py
+++ bios/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Bio(models.Model):
+    """
+    Player or anybody else bio.
+    """
+    
+    name = models.CharField(max_length=500)
+    height = models.IntegerField(null=True, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    birthplace = models.CharField(max_length=250, null=True, blank=True)
+
+    #source = models.CharField(max_length=500)
+
+    class Meta:
+        ordering = ('-birthdate',)
+        
+    def __unicode__(self):
+        return self.name
+
+        
+        
+
--- games/models.py
+++ games/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+from s2.teams.models import Team
+
+class Game(models.Model):
+    """
+    Represents a completed game.
+    """
+    # Should we have a date and a datetime field?
+    date = models.DateField()
+    
+    home_team = models.ForeignKey(Team)
+    home_score = models.IntegerField()
+
+    away_team = models.ForeignKey(Team)
+    away_score = models.IntegerField()
+
+    # This should probably be a many-to-many.
+    competition = models.CharField(max_length=255)
+
+    location = models.CharField(max_length=255)
+
+    notes = models.TextField()
+
+    attendance = models.IntegerField(null=True, blank=True)
+    referee = models.CharField(max_length=255)
+
+
+    class Meta:
+        ordering = ('date',)
+        unique_together = [('home_team', 'date'), ('away_team', 'date')]
+
+
+    def score(self):
+        return "%s - %s" % (self.home_score, self.away_score)
+
+
+    def __unicode__(self):
+        return u"%s: %s v %s" % (self.date, self.home_team, self.away_team)
+
+
+
