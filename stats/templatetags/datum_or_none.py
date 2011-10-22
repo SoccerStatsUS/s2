8b347a477aa1b12b7368a87dd368fd58fa306728
Chris Edgemon <chrisedgemon@gmail.com>
2011-10-22 19:10:59 -0400

    Add templatetag to grey fields.

---
+++ stats/templatetags/datum_or_none.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag('datum_or_grey.html')
+def datum_or_grey(datum):
+    return {
+        'datum': datum,
+        'is_none': datum is None
+        }
