90e8feb77600d63c63bd96b380693f4737d6887e
Chris Edgemon <chrisedgemon@gmail.com>
2011-11-01 14:08:50 -0400

    Add birthdates, hirings, firings to dates page.

--- dates/urls.py
+++ dates/urls.py
@@ -3,16 +3,16 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('s2.dates.views', 
 
                        url(r'^(?P<year>\d\d\d\d)$',
-                           'year_scoreboard',
-                           name='year_scoreboard'),
+                           'year_detail',
+                           name='year_detail'),
 
                        url(r'^(?P<year>\d\d\d\d)/(?P<month>\d\d)$',
-                           'month_scoreboard',
-                           name='month_scoreboard'),
+                           'month_detail',
+                           name='month_detail'),
 
                        url(r'^(?P<year>\d\d\d\d)/(?P<month>\d\d)/(?P<day>\d\d)$',
-                           'date_scoreboard',
-                           name='date_scoreboard'),
+                           'date_detail',
+                           name='date_detail'),
 
 
                        )
--- dates/views.py
+++ dates/views.py
@@ -3,24 +3,43 @@ import datetime
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
+
+from s2.bios.models import Bio
 from s2.games.models import Game
+from s2.positions.models import Position
 
-def year_scoreboard(request, year):
+def year_detail(request, year):
     # Add a paginator.
+    year = int(year)
     games = Game.objects.filter(date__year=int(year))
+    births = Bio.objects.filter(birthdate__year=year).order_by('birthdate')
+    hires = Position.objects.filter(start__year=year)
+    fires = Position.objects.filter(end__year=year)
+
     context = {
         'games': games,
+        'births': births,
+        'hires': hires,
+        'fires': fires,
         }
     return render_to_response("games/list.html",
                               context,
                               context_instance=RequestContext(request))
 
 
-def month_scoreboard(request, year, month):
+def month_detail(request, year, month):
     # Add a paginator.
-    games = Game.objects.filter(date__year=int(year), date__month=int(month))
+    year, month = int(year), int(month)
+    games = Game.objects.filter(date__year=year, date__month=month)
+    births = Bio.objects.filter(birthdate__year=year, birthdate__mont=month).order_by('birthdate')
+    hires = Position.objects.filter(start__year=year, start__month=month)
+    fires = Position.objects.filter(end__year=year, end__month=month)
+
     context = {
         'games': games,
+        'births': births,
+        'hires': hires,
+        'fires': fires,
         }
     return render_to_response("games/list.html",
                               context,
@@ -28,12 +47,18 @@ def month_scoreboard(request, year, month):
 
 
 
-def date_scoreboard(request, year, month, day):
+def date_detail(request, year, month, day):
     # Add a paginator.
     d = datetime.date(int(year), int(month), int(day))
     games = Game.objects.filter(date=d)
+    births = Bio.objects.filter(birthdate=d).order_by('birthdate')
+    hires = Position.objects.filter(start=d)
+    fires = Position.objects.filter(end=d)
     context = {
         'games': games,
+        'births': births,
+        'hires': hires,
+        'fires': fires,
         }
     return render_to_response("games/list.html",
                               context,
