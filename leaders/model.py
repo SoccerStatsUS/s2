e4a3bf66b287e0cbb63d5ddfa2e629937e93b3dc
Chris Edgemon <chrisedgemon@gmail.com>
2011-11-28 18:44:16 -0500

    Add leaders app.

---
+++ leaders/model.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+
+from s2.teams.models import Team
+from s2.bios.models import Bio
+from s2.competitions.models import Competition, Season
+
+    
+
+
+
