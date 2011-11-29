e069d60662cb459745c91037130de5505e15e6d2
chirs <chrisedgemon@gmail.com>
2011-11-28 21:24:28 -0500

    Improve dates urls.

--- dates/urls.py
+++ dates/urls.py
@@ -2,15 +2,15 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('s2.dates.views', 
 
-                       url(r'^(?P<year>\d\d\d\d)$',
+                       url(r'^(?P<year>\d+)$',
                            'year_detail',
                            name='year_detail'),
 
-                       url(r'^(?P<year>\d\d\d\d)/(?P<month>\d\d)$',
+                       url(r'^(?P<year>\d+)/(?P<month>\d+)$',
                            'month_detail',
                            name='month_detail'),
 
-                       url(r'^(?P<year>\d\d\d\d)/(?P<month>\d\d)/(?P<day>\d\d)$',
+                       url(r'^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)$',
                            'date_detail',
                            name='date_detail'),
 
