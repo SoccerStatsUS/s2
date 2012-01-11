873910ff3204d56efe3fcb3a1cef5a61aa1829ee
chirs <chrisedgemon@gmail.com>
2012-01-10 23:58:52 -0500

    Change bio SearchIndex to use ModelSearchIndex.

--- bios/search_indexes.py
+++ bios/search_indexes.py
@@ -1,17 +1,13 @@
 import datetime
 
-from haystack.indexes import *
+from haystack.indexes import ModelSearchIndex
 from haystack import site
 from s2.bios.models import Bio
 
-class BioIndex(SearchIndex):
-    text = CharField(document=True)
-    birthplace = CharField(model_attr='birthplace')
-    
-    def index_queryset(self):
-        """Used when the entire index for model is updated."""
-        #return Note.objects.filter(pub_date__lte=datetime.datetime.now())
-        return Bio.objects.all()
+class BioIndex(ModelSearchIndex):
+
+    class Meta:
+        fields = ['name', 'birthplace']
 
 site.register(Bio, BioIndex)
     
