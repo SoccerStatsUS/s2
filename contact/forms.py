e44ec99a028e22746c8619b7ec65399b72bf2ed9
chirs <chrisedgemon@gmail.com>
2012-01-14 23:34:42 -0500

    Change label text on contact form.

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    email = forms.EmailField(required=False)
+    email = forms.EmailField(required=False, label="Email (not required)")
     message = forms.CharField(widget=forms.Textarea())
 
