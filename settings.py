67b7ae866184b9f13b8d7bb8e0ba7e100aeced02
chirs <chrisedgemon@gmail.com>
2012-07-31 18:35:00 -0400

    Remove custom_settings from repo, move most of its content into settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+import os
 from os.path import join
 import socket
 
@@ -5,6 +6,42 @@ from secret_settings import *
 from custom_settings import *
 
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
+
+
+#### Put this part anywhere else?
+# Make sure to use a trailing slash for MEDIA_URL
+
+if os.path.exists("/Users"):
+    PROJECT_ROOT = "/Users/chrisedgemon"
+    PROJECT_DIRNAME = 's2'
+elif DEBUG:
+    PROJECT_ROOT = "/home/chris"
+    PROJECT_DIRNAME = 'sdev'
+else:
+    PROJECT_ROOT = "/home/chris"
+    PROJECT_DIRNAME = 's2'
+
+if DEBUG:
+    CACHE_MIDDLEWARE_KEY_PREFIX = "dev:"
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+            }
+}
+
+else:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211',
+            }
+        }
+
+
+PROJECT_DIR = "%s/www/%s" % (PROJECT_ROOT, PROJECT_DIRNAME)
+DB_PATH = "%s/www/%s/db/soccer.db" % (PROJECT_ROOT, PROJECT_DIRNAME)
+#### Put this part anywhere else?
+
 
 PRODUCTION_SITES = (
     "bert",
