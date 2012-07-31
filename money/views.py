ff79c34d8f2aa61d8db51495017f35375bca707a
chirs <chrisedgemon@gmail.com>
2012-07-31 18:29:40 -0400

    Add money app to project - for salaries and stuff.

---
+++ money/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.views.decorators.cache import cache_page
+
+from bios.models import Bio
+from competitions.models import Competition, Season
+from drafts.models import Draft
+
+
+@cache_page(60 * 60 * 12)
+def drafts_index(request):
+    """
+    List all drafts.
+    """
+
+    context = {
+        'drafts': Draft.objects.all()
+        }
+    return render_to_response("drafts/index.html",
+                              context,
+                              context_instance=RequestContext(request))
+
+
+@cache_page(60 * 60 * 12)
+def draft_detail(request, competition_slug, draft_slug, season):
+    """
+    Draft detail page. Don't want to use competition since some drafts don't have a competition?
+    """
+    competition = get_object_or_404(Competition, slug=competition_slug)
+    draft = get_object_or_404(Draft, season=season, slug=draft_slug)
+
+    context = {
+        'draft': draft,
+        }
+    return render_to_response("drafts/detail.html",
+                              context,
+                              context_instance=RequestContext(request))
+
+
+
+
+
+def big_board(request):
+    drafts = Draft.objects.filter(name__contains='USMNT')
+    
+    context = {
+        'drafts': drafts,
+        }
+
+    return render_to_response('drafts/bigboard.html',
+                              context,
+                              context_instance=RequestContext(request))
+
+
+def draft_person_ajax(request, slug):
+    context = {
+        'player': Bio.objects.get(slug=slug)
+        }
+    return render_to_response('drafts/ajax.html',
+                              context,
+                              context_instance=RequestContext(request))
