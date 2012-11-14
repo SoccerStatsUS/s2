43be4014cce889960e8ad33cc9f8acc6fafad59e
chirs <chrisedgemon@gmail.com>
2012-11-14 01:12:09 -0500

    Update update_prod.sh

--- update_prod.sh
+++ update_prod.sh
@@ -1,6 +1,8 @@
-
 export PGPASSWORD=ymctas
 
 dropdb soccerstats
 psql -U soccerstats -d soccerstats_backup -c "CREATE DATABASE soccerstats WITH TEMPLATE soccerstats_dev"
 
+sudo stop s2prod
+sudo start s2prod
+sudo /etc/init.d/memcached restart
