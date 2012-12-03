f3419cea4515f9b5a474dfb605437512a9dcc806
chirs <chrisedgemon@gmail.com>
2012-12-03 14:03:15 -0500

    Make contact form csrf exempt for ease of use everywhere.

--- contact/views.py
+++ contact/views.py
@@ -3,9 +3,11 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
 
 from contact.forms import ContactForm
 
+@csrf_exempt
 def contact_index(request):
 
     if request.method == 'POST':
