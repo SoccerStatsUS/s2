a18ff5f1ba9b526dac70cbef9d63d53c0c625619
chirs <chrisedgemon@gmail.com>
2013-08-02 15:07:51 -0400

    Add simple images app.

---
+++ images/models.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+import datetime
+import os
+
+from django.contrib.contenttypes import generic
+from django.core.urlresolvers import reverse
+from django.db import models
+from django.template.defaultfilters import slugify
+
+
+
+
+class Image(models.Model):
+
+    name = models.CharField(max_length=200, unique=True)
+
+    # Affiliate team. e.g.
+    # Portland Timbers Reserves -> Portland Timbers (reserve -> main)
+    # Chicago Fire Select -> Chicago Fire (? affiliation)
+    # United States -> United States U-20 (youth team)
+    affiliate = models.ForeignKey('self', null=True)
+
+    src = models.ImageField('self', null=True)
+
+    
+
+
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    target = generic.GenericForeignKey()
+
--- settings.py
+++ settings.py
@@ -178,6 +178,7 @@ INSTALLED_APPS = (
     'games',
     'goals',
     'graphs',
+    'images',
     'levels',
     'lineups',
     'money',
