c35a133e6987230b5d1b712f81c81fbca81ea0ea
chirs <chrisedgemon@gmail.com>
2013-12-05 18:01:19 -0500

    Work on new templates.

--- new-static/js/custom.js
+++ new-static/js/custom.js
@@ -3,8 +3,16 @@
 
   $(document).ready(function() {
 
-      $("#menu-button").click(function(){
-          $("#menu").show();
+      $("#header").click(function(){
+          var m = $("#menu");
+          var mb = $("#menu-button");
+          var display = ((m.css("display") === "none") ? "block" : "none");
+          m.css("display", display);
+          if (mb.css("color") === "rgb(255, 255, 255)"){
+          mb.css("color", "#000").css("background-color", "#fff");
+          } else {
+          mb.css("color", "#fff").css("background-color", "#000");
+          }
       });
 
   });
