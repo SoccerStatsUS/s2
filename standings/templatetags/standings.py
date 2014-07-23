7e8dc1a3b824234ecf6bda66277ba46f1aab9a60
chirs <chrisedgemon@gmail.com>
2014-07-22 22:13:27 -0400

    fix links in templates/homepage.html; fix standings templatetag for stadiums.

--- standings/templatetags/standings.py
+++ standings/templatetags/standings.py
@@ -1,9 +1,12 @@
 from django import template
+from django.db.models.base import FieldError
 
 register = template.Library()
 
 @register.inclusion_tag('templatetags/standings.html')
 def standings_table(standings, exclude=''):
+
+    has_value = lambda i: set(e[0] for e in vals) != set([None])
 
     try:
         has_points = standings.exclude(points=None).exists()
@@ -12,8 +15,11 @@ def standings_table(standings, exclude=''):
     except AssertionError:
         # this seems incorrect
         vals = standings.values_list('points', 'ties')
-        has_value = lambda i: set(e[0] for e in vals) != set([None])
         has_points, has_ties = [has_value(e) for e in range(2)]
+    except FieldError:
+        vals = standings.values_list('ties')
+        has_ties = has_value(vals)
+        has_points = False
 
 
     return {
