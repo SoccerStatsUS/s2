973bd95eb7984a79bc8cdbad7ec53754f33a0107
Chris Edgemon <chrisedgemon@gmail.com>
2014-08-20 22:28:36 +0000

    Also create soccerstats_dev db

--- build.sh
+++ build.sh
@@ -8,12 +8,13 @@ dropdb soccerstats_build
 createdb soccerstats_build --owner=soccerstats
 createdb soccerstats_backup --owner=soccerstats
 createdb soccerstats --owner=soccerstats
-python manage.py syncdb --noinput --settings=build_settings
-python build/load.py 1
-python build/load.py 2
-python build/load.py 3
-python build/load.py 4
-python build/generate.py
+createdb soccerstats_dev --owner=soccerstats
+python3 manage.py syncdb --noinput --settings=build_settings
+python3 build/load.py 1
+python3 build/load.py 2
+python3 build/load.py 3
+python3 build/load.py 4
+python3 build/generate.py
 
 dropdb soccerstats_backup
 psql -U soccerstats -d soccerstats_build -c "ALTER DATABASE soccerstats_dev RENAME TO soccerstats_backup"
