c1f7e725f3c1dce7f6ed10c14f39b9fe008f2b3d
chirs <chrisedgemon@gmail.com>
2026-08-01 22:42:09 -0600

    Source pages: show top 25 player-season stats when the source has stats

--- sources/views.py
+++ sources/views.py
@@ -1,10 +1,11 @@
 import datetime
 
-from django.db.models import Count, Q
+from django.db.models import Count, F, Q
 from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext
 
 from sources.models import Source
+from stats.models import Stat
 
 
 def source_index(request):
@@ -22,9 +23,16 @@ def source_index(request):
 
 def source_detail(request, source_id):
     source = get_object_or_404(Source, id=source_id)
+
+    stats = Stat.objects.filter(source=source)
+    stats_count = stats.count()
+    top_stats = stats.order_by(F('games_played').desc(nulls_last=True)).select_related()[:25]
+
     context = {
         'source': source,
         'feeds': source.feeditem_set.order_by('-dt'),
+        'stats_count': stats_count,
+        'top_stats': top_stats,
         }
     return render(request, "sources/detail.html",
                               context)
